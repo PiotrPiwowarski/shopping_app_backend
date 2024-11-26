8e72dc7e626693db31ca56968ea776bb91950034
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-11-26 20:50:29 +0100

    new commit

--- src/main/java/com/example/shopping_app_backend/config/SecurityConfiguration.java
+++ src/main/java/com/example/shopping_app_backend/config/SecurityConfiguration.java
@@ -13,6 +13,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,11 +44,24 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .logout((logout) -> logout.logoutUrl("/api/users/logout")
                         .addLogoutHandler(logoutHandler)
                         .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,13 @@
 INSERT INTO USERS (email, password, role) VALUES ('admin@gmail.com', '$2y$10$STKqUJ3v5miGEYFNrb4GyuUItn4s2CJYNMnZ1ZDjQChGh.NjDrFzO', 'USER'); /*password: admin*/
 
 INSERT INTO ITEMS (shop, product_name, price, amount, description, image_url, user_id)
-VALUES ('Example Shop', 'Example Product', 99.99, 1, 'This is a sample item description', 'http://example.com/image.jpg', 1);
+VALUES ('Biedronka', 'Mleko', 4.99, 1, 'Dobre mleko', 'https://res.cloudinary.com/dj484tw6k/f_auto,q_auto,c_pad,b_white,w_557,h_557/v1548061372/be/3268.jpg', 1);
 
 INSERT INTO ITEMS (shop, product_name, price, amount, description, image_url, user_id)
-VALUES ('Lidl', 'Milk', 5.59, 3, 'Milk is very important for our health', 'https://cdn.britannica.com/77/200377-050-4326767F/milk-splashing-glass.jpg', 1);
+VALUES ('Lidl', 'Mleko', 5.59, 3, 'Mleko jest bardzo zdrowe dla zdrowia', 'https://cdn.britannica.com/77/200377-050-4326767F/milk-splashing-glass.jpg', 1);
+
+INSERT INTO ITEMS (shop, product_name, price, amount, description, image_url, user_id)
+VALUES ('Lidl', 'Kabanosy', 9.69, 7, 'Duża paczka kabanosów', 'https://www.zmlukow.pl/wp-content/uploads/2015/05/kabanosy_lukowskie_smaki_ok1kg_180g_nowa_etykieta_lukow-2048x1536.jpg', 1);
+
+INSERT INTO ITEMS (shop, product_name, price, amount, description, image_url, user_id)
+VALUES ('Carrefour', 'Chleb', 6.99, 2, 'Chleb 1kg razowy', 'https://wiejskiewypieki.pl/hpeciai/eb02a5704392ea318d740c97ed57487a/pol_pl_Chleb-razowy-na-naturalnym-zakwasie-500-g-84_1.jpg', 1);
